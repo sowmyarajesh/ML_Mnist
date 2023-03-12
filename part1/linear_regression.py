ef0cce8e7e3069ce2cd70a3c87e348ef885b8597
Sowmya R <soumya80@gmail.com>
2023-03-12 12:52:47 -0400

    Update linear_regression.py

--- part1/linear_regression.py
+++ part1/linear_regression.py
@@ -1,7 +1,18 @@
 import numpy as np
 
 ### Functions for you to fill in ###
-
+def IdentityMatrix(n):
+    imat = []
+    for r in range(n):
+        row = []
+        for c in range(n):
+            if r==c:
+                row.append(1)
+            else:
+                row.append(0)
+        imat.append(row)
+    return imat
+    
 def closed_form(X, Y, lambda_factor):
     """
     Computes the closed form solution of linear regression with L2 regularization
@@ -15,10 +26,13 @@ def closed_form(X, Y, lambda_factor):
         theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
         represents the y-axis intercept of the model and therefore X[0] = 1
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    I = IdentityMatrix(X.shape[1])    
+    step1 = np.dot(X.T,X)
+    step2 = np.add(step1, np.dot(lambda_factor, I))
+    step3 = np.linalg.inv(step2)
+    theta = np.dot(step3, np.dot(X.T,Y))
+    return theta
 
-### Functions which are already complete, for you to use ###
 
 def compute_test_error_linear(test_x, Y, theta):
     test_y_predict = np.round(np.dot(test_x, theta))
